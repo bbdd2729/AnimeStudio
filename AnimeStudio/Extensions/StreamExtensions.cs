2634eb5e32c3067fbe03e414dcfccd0fa659d327
bbdd <Love4Yuihirasawa@gmail.com>
2026-06-29 19:04:49 +0800

    Pool stream copy buffers

--- AnimeStudio/Extensions/StreamExtensions.cs
+++ AnimeStudio/Extensions/StreamExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Buffers;
+using System.IO;
 
 namespace AnimeStudio
 {
@@ -8,16 +9,23 @@ namespace AnimeStudio
 
         public static void CopyTo(this Stream source, Stream destination, long size)
         {
-            var buffer = new byte[BufferSize];
-            for (var left = size; left > 0; left -= BufferSize)
+            var buffer = ArrayPool<byte>.Shared.Rent(BufferSize);
+            try
             {
-                int toRead = BufferSize < left ? BufferSize : (int)left;
-                int read = source.Read(buffer, 0, toRead);
-                destination.Write(buffer, 0, read);
-                if (read != toRead)
+                for (var left = size; left > 0; left -= BufferSize)
                 {
-                    return;
+                    int toRead = BufferSize < left ? BufferSize : (int)left;
+                    int read = source.Read(buffer, 0, toRead);
+                    destination.Write(buffer, 0, read);
+                    if (read != toRead)
+                    {
+                        return;
+                    }
                 }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
             }
         }
 
